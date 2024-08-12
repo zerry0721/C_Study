ef68be26adad64c0d6c225a7cc19e795ebd00a63
raffin <zerry0721@gmail.com>
2024-08-12 18:19:44 +0900

    help

--- hello.c
+++ hello.c
@@ -5,6 +5,3 @@ int main() //C 언어 프로그램에서 가장 처음 실행되는 함수
     printf("Hello, world!\n");
     return 0; //함수 바깥으로 값을 전달하는 반환값 부분
 }
-//commit test
-//commit test2
-//commit test3
---
+++ printfscanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    const int YEAR = 2000;
+    printf("태어난 년도 : %d\n", YEAR);
+    //YEAR = 2001;
+    return 0;
+}
