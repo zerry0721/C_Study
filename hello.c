1b3a6b2b9dc01c622eec3d8946f899aee24bb553
raffin <zerry0721@gmail.com>
2024-08-12 17:51:01 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //printf 함수를 사용할 때 필요한 헤더파일
+
+int main() //C 언어 프로그램에서 가장 처음 실행되는 함수
+{
+    printf("Hello, world!\n");
+    return 0; //함수 바깥으로 값을 전달하는 반환값 부분
+}
---
+++ hello_with_format.c
@@ -0,0 +1,6 @@
+//서식 지정자는 printf로 문자열을 출력할 때 값으로 바뀌는 부분
+#include <stdio.h>
+int main(){
+    printf("%s %s\n", "Hello,", "1234");
+    return 0;
+}
